6e826c92ea9a6350641b64b849dc7a540df9e76f
Kim Hsiao <white.xtera@gmail.com>
2013-04-05 14:44:21 +0800

    add some examples

--- data-structure/dictionary1.py
+++ data-structure/dictionary1.py
@@ -31,3 +31,22 @@ print 'guido' in tel
 True
 also may check if the key inside the dictionary with this easy way
 '''
+
+dict = dict([('sape', 4139), ('guido', 4127), ('jack', 4098)])
+print dict
+''' will return
+{'sape': 4139, 'jack': 4098, 'guido': 4127}
+with dict() will convert the pair data to dictionary
+'''
+
+res = {x: x**2 for x in (2, 4, 6)}
+print res
+''' will return
+{2: 4, 4: 16, 6: 36}
+it's also a convinent way if you want to define the key/value dictionary as numeric data
+'''
+
+#print dict(sape=4139, guido=4127, jack=4098)
+''' fail
+it should return a dictionary as the result above, but it fail
+'''
