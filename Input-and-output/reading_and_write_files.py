2bb2288c8a8f04a799122cda1a95042a413f3d2e
Kim Hsiao <white.xtera@gmail.com>
2013-04-09 11:35:50 +0800

    Completed input and output chapter. also add one sample pure txt file which named as workfile

--- Input-and-output/reading_and_write_files.py
+++ Input-and-output/reading_and_write_files.py
@@ -39,3 +39,32 @@ f = open('sample.txt', 'r')
 print f.readlines()
 # this will read all the lines of the file and return as a list
 f.close()
+
+#===============================================================================
+# new study for file seek
+#===============================================================================
+f = open('workfile', 'r+')
+f.write('0123456789abcdef')
+f.seek(5)  # go to the 6th byte of the file
+print f.read(1)  # will show "5"
+
+f.seek(-3, 2)  # go to the 3rd byte before the end
+print f.read(1)  # will show "d"
+
+f.close()
+
+# f.read()
+'''
+error message will raise since we have close the file already.
+'''
+
+with open('workfile', 'r') as f:
+    read_data = f.read()
+f.closed
+'''
+I think it's a better way to access the file, since we have wrapped all the code within the with statement.
+I think it's much readable.
+'''
+
+print read_data  # will show "0123456789abcdef"
+print f  # will show "<closed file 'workfile', mode 'r' at 0x025BB700>"
